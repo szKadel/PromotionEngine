d36669e5b87c209a7c538ee364b4d123578e9a02
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-09-12 17:21:25 +0200

    VacationLimits -> view -> Vacation

--- src/Entity/Company/Employee.php
+++ src/Entity/Company/Employee.php
@@ -63,7 +63,7 @@ class Employee
     private ?Department $department = null;
 
     #[ORM\OneToMany(mappedBy: 'Employee', targetEntity: VacationLimits::class, orphanRemoval: true)]
-    #[Groups(['employee:read'])]
+    #[Groups(['employee:read','vacationLimit:read'])]
     private Collection $vacationLimits;
 
     #[ORM\OneToMany(mappedBy: 'employee', targetEntity: Vacation::class)]
