f150a1071e4c779060f95dba30bf876d6005523a
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-09-15 16:13:49 +0200

    Test -> functional -> Vacation

--- public/index.php
+++ public/index.php
@@ -2,14 +2,6 @@
 
 use App\Kernel;
 
-    header('Access-Control-Allow-Origin: *');
-
-
-    header('Access-Control-Allow-Headers:*');
-    header('Access-Control-Allow-Credentials:true');
-    header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
-
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
