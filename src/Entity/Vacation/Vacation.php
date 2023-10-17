741c34058789f3a98431aa3aa18a5baf36f3755d
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-10-17 17:16:05 +0200

    NotificationEmail System

--- src/Entity/Vacation/Vacation.php
+++ src/Entity/Vacation/Vacation.php
@@ -42,7 +42,7 @@ use Symfony\Component\Validator\Constraints as Assert;
     paginationItemsPerPage: 7,
 )]
 #[ApiFilter(\ApiPlatform\Doctrine\Orm\Filter\SearchFilter::class,properties: ['employee.department'=>'exact'])]
-#[ApiFilter(OrderFilter::class, properties: ['vacation.id','vacation.createdAt','vacation.createdAt'])]
+#[ApiFilter(OrderFilter::class, properties: ['id','createdAt','createdAt'])]
 class Vacation
 {
     #[ORM\Id]
