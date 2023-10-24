e4f6f497bbec7611471385429f120797859fb126
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-10-24 17:08:34 +0200

    Vacation -> Employee -> getEmployeeOnVacation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,30 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Schema\View;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Twig\Environment;
 
-class MainController
+class MainController extends AbstractController
 {
+    public function __construct(
+        private Environment $twig
+    )
+    {
+
+    }
+
     #[Route('/',name: 'app_main_mainpage')]
     public function mainPage(): RedirectResponse
     {
         return new RedirectResponse('/view/');
+    }
+
+    #[Route('/test')]
+    public function test(): \Symfony\Component\HttpFoundation\Response
+    {
+        return  $this->render("email/notification/adminNewVacation.html.twig");
     }
 }
