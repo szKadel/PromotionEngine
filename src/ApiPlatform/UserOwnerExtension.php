2415e548b4f8ad70bf2a5fcbf7674fdc578ae5d7
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-11-06 15:40:28 +0100

    More Departments for Mod -> creating tables

--- src/ApiPlatform/UserOwnerExtension.php
+++ src/ApiPlatform/UserOwnerExtension.php
@@ -6,7 +6,11 @@ use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Entity\Company\Employee;
+use App\Entity\EmployeeExtendedAccesses;
 use App\Entity\Vacation\Vacation;
+use App\Repository\EmployeeExtendedAccessesRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
@@ -14,7 +18,7 @@ use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusExce
 final class UserOwnerExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
 
-    public function __construct(private readonly Security $security)
+    public function __construct(private readonly Security $security, private EmployeeExtendedAccessesRepository $extendedAccessesRepository)
     {
     }
 
@@ -47,20 +51,39 @@ final class UserOwnerExtension implements QueryCollectionExtensionInterface, Que
         $queryBuilder->setParameter('current_user_id', $user->getEmployee()->getId());
     }
 
-    public function groupModerator(QueryBuilder $queryBuilder, string $resourceClass)
-    {
-        if (Vacation::class !== $resourceClass || $this->security->isGranted('ROLE_ADMIN')||$this->security->isGranted('ROLE_KADR')|| null === $user = $this->security->getUser()) {
+    public function groupModerator(QueryBuilder $queryBuilder, string $resourceClass) {
+        if ($resourceClass !== Vacation::class || $this->hasSufficientPermissions() || !$user = $this->security->getUser()) {
             return;
         }
 
-        if($this->security->isGranted('ROLE_MOD')){
-            $rootAlias = $queryBuilder->getRootAliases()[0];
-
-            $queryBuilder->join(sprintf('%s.employee', $rootAlias), 'u');
-            $queryBuilder->andWhere('u.department = :department');
-            $queryBuilder->setParameter('department', $user->getEmployee()->getDepartment());
-
-
+        if ($this->security->isGranted('ROLE_MOD')) {
+            $this->applyDepartmentFilters($queryBuilder);
         }
+    }
+
+    private function hasSufficientPermissions() {
+        return $this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_KADR');
+    }
+
+    private function applyDepartmentFilters(QueryBuilder $queryBuilder) {
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->join(sprintf('%s.employee', $rootAlias), 'u');
+        $queryBuilder->andWhere('u.department = :department');
+
+        $accessDepartments = $this->getExtendedAccess($this->security->getUser()->getEmployee());
+
+        $departmentParams = [];
+        foreach ($accessDepartments as $index => $departmentAccess) {
+            $key = ":department" . $index;
+            $queryBuilder->orWhere('u.department = ' . $key);
+            $queryBuilder->setParameter($key, $departmentAccess->getDepartment());
+            $departmentParams[] = $departmentAccess->getDepartment();
+        }
+
+        $queryBuilder->setParameter('department', $this->security->getUser()->getEmployee()->getDepartment());
+    }
+
+    private function getExtendedAccess($employee) {
+        return $this->extendedAccessesRepository->findBy(['employee' => $employee]);
     }
 }
