7ec952379c15c5b15ecc997f44be1eb4d008c915
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-11-27 12:23:27 +0100

    Reset Password by User & Vacation days from previous year

--- src/Repository/VacationRepository.php
+++ src/Repository/VacationRepository.php
@@ -38,10 +38,7 @@ class VacationRepository extends ServiceEntityRepository
 
         $result = $this->createQueryBuilder('v')
             ->andWhere('v.employee = :employee')
-            ->andWhere('(v.status = :status or v.status = :status2)')
             ->andWhere('(:dateFrom BETWEEN v.dateFrom AND v.dateTo OR :dateTo BETWEEN v.dateFrom AND v.dateTo OR v.dateFrom BETWEEN :dateFrom AND :dateFrom OR v.dateFrom BETWEEN :dateFrom AND :dateTo OR :dateFrom = v.dateTo OR v.dateTo = :dateFrom OR v.dateFrom = :dateTo)')
-            ->setParameter('status', $statusAccepted)
-            ->setParameter('status2', $statusWaiting)
             ->setParameter('employee', $employee)
             ->setParameter('dateFrom', $startDate->format('Y-m-d'))
             ->setParameter('dateTo', $endDate->format('Y-m-d'))
