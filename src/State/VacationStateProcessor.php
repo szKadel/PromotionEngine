1518f0d06ff610e47081b8263b279e860c898693
Szymon Kadelski <kontakt@szymonkadelski.pl>
2023-11-27 13:21:04 +0100

    Reset Password by User & Vacation days from previous year

--- src/State/VacationStateProcessor.php
+++ src/State/VacationStateProcessor.php
@@ -54,7 +54,7 @@ class VacationStateProcessor implements ProcessorInterface
                     throw new BadRequestException("Pracownika dla tego wniosku jest dezaktywowany.");
                 }
 
-                if($data->getReplacement()?->getUnActive()){
+                if(!empty($data->getReplacement()?->getUnActive())){
                     throw new BadRequestException("Pracownik na zastępstwie jest dezaktywowany.");
                 }
 
