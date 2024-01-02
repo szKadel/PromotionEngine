472cac8684a26745fd94f156f1f9ebe97da2d760
Szymon <kontakt@szymonkadelski.pl>
2024-01-02 13:57:54 +0100

    Refactor

--- tests/Functional/Api/YearSummarationTest.php
+++ tests/Functional/Api/YearSummarationTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Functional\Api;
 
+use App\Factory\Company\DepartmentFactory;
+use App\Factory\Company\EmployeeFactory;
 use App\Factory\VacationTypesFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
@@ -15,6 +17,9 @@ class YearSummarationTest extends KernelTestCase
     public function testCountVacationSumaration()
     {
         $vacationType = VacationTypesFactory::createOne(["name"=>"Urlop Wypoczynkowy"]);
+
+        $department = DepartmentFactory::createOne();
+        $employee = EmployeeFactory::createOne(['department'=>$department]);
 
         $this->browser()->get('/Vacations/YearSummation/2023')->assertStatus(200);
     }
