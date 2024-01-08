927cc85aa1b9e01554cd156fd286442576fefb98
Szymon Kadelski <kontakt@szymonkadelski.pl>
2024-01-08 17:25:03 +0100

    Vacation Callendar -> view Controller

--- src/Controller/Entity/VacationController.php
+++ src/Controller/Entity/VacationController.php
@@ -69,7 +69,7 @@ class VacationController extends AbstractController
     ): JsonResponse
     {
 
-        if(!$this->security->isGranted('ROLE_ADMIN')){
+        if(!$this->security->isGranted('ROLE_ADMIN') | !$this->security->isGranted('ROLE_KADR')){
             $department_id = $user->getEmployee()->getDepartment()->getId();
         }
 
